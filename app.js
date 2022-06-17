e37167988f7b9aeb812a7fa9152484e0bafe51f8
Hugh Kwabiah <hughkwabiah@gmail.com>
2022-06-17 14:39:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+
+// let yourAge = prompt("Please enter your age.");
+
+// if (yourAge > 18) {
+//   document.write("<p>You are old enough to enter this website.</p>");
+// }
+// else {
+//   document.write("<p>Unfortunately you cannot enter this website.</p>");
+// }
+
+// let favFtbTeam = prompt("Please enter your favourite football team.");
+
+// if (favFtbTeam.toUpperCase() == "ARSENAL") {
+//   document.write("<p>Great choice.</p>");
+// }
+
+// else if (favFtbTeam.toUpperCase() == "LIVERPOOL") {
+//   document.write("<p>Not bad.</p>");
+// }
+
+// else if (favFtbTeam.toUpperCase() == "TOTENHAM" || favFtbTeam.toUpperCase() == "HOTSPURS" || favFtbTeam.toUpperCase() == "SPURS") {
+//   document.write("<p>Worst decision possible, north London is Red.</p>");
+// }
+
+// else if (favFtbTeam.toUpperCase() == "MANCHESTER CITY" || favFtbTeam.toUpperCase() == "CITY" || favFtbTeam.toUpperCase() == "MAN CITY") {
+//   document.write("<p>Who's favourite team is Man city?</p>");
+// }
+
+// else {
+//   document.write("<p>Should've picked Arsenal</p>");
+// }
+
+// let fruit = "lychee";
+
+// const topTierFruit = ["pear","grape","mango","pineapple","watermelon","orange","strawberry","peach","lychee"];
+// const midTierFruit = ["apple","plum","kiwi","cherry","banana","melon"];
+
+// if (topTierFruit.includes(fruit)) {
+//   console.log(`${fruit} is a top tier fruit.`);
+// }
+// else if (midTierFruit.includes(fruit)) {
+//   console.log(`${fruit} is a mid tier fruit.`);
+// }
+// else {
+//   console.log(`${fruit} is a low tier fruit.`);
+// }
+
+// if (1 === 1 || 2 === 3) {
+//   console.log("this has worked");
+// } else {
+//   console.log("this has not worked");
+// } 
+
+// truthy or falsy
+// falsy = false, 0, -0, "", null, undefined, NaN
+
+// let var1 = 0;
+
+// if (var1) {
+//   console.log("this has worked");
+// } else {
+//   console.log("this has not worked");
+// }
+
+// ternary operator
+
+// const varTernary = true;
+// console.log(
+//   varTernary ? "this is the true result" : "this is the false result"
+// );
